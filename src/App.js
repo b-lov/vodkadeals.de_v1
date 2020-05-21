fad998bb7b876cf949a23ad395a26942e25f938b
Max B <maxbilov@gmail.com>
2020-05-21 12:18:48 +0200

    pass relevant data from app to episode item

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 // vim: ft=javascriptreact
 import React from 'react';
 import './App.css';
+import EpisodeItem from './components/EpisodeItem';
 import Parser from 'rss-parser';
 let parser = new Parser();
 
 export default class App extends React.Component {
-  state = { feed: [] };
+  state = {
+    feed: [],
+    loading: true,
+  };
 
   async componentDidMount() {
     const feed = await parser.parseURL('https://pinecast.com/feed/vodkadeals');
-    this.setState({ feed });
+    this.setState({ feed, loading: false });
   }
 
   render() {
-    const { feed } = this.state;
-    console.log(feed);
-    return <div>{feed.description}</div>;
+    const { feed, loading } = this.state;
+    // !loading && console.log(feed.items);
+    if (loading) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <EpisodeItem data={feed.items[21]} />
+        </div>
+      );
+    }
   }
 }

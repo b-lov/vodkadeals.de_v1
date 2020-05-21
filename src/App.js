a9fea6fdecc0488fb6f11ea3f1964805fa264915
Max B <maxbilov@gmail.com>
2020-05-21 10:07:20 +0200

    cleanup and import rss feed into app state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+// vim: ft=javascriptreact
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Parser from 'rss-parser';
+let parser = new Parser();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = { feed: [] };
+
+  async componentDidMount() {
+    const feed = await parser.parseURL('https://pinecast.com/feed/vodkadeals');
+    this.setState({ feed });
+  }
+
+  render() {
+    const { feed } = this.state;
+    console.log(feed);
+    return (
+      <div>{feed.description}</div>
+      // {/* <div> */}
+      // {/*     <h1>RSS Feed</h1> */}
+      // {/*     this.state.feed.map((item, i) => ( */}
+      // {/*         <div key={i}> */}
+      // {/*             <h1>item.title</h1> */}
+      // {/*             <a href="">item.link</a> */}
+      // {/*         </div> */}
+      // {/*     )) */}
+      // {/* </div> */}
+    );
+  }
 }
-
-export default App;

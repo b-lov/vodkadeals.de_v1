7d76a126df0428392e20e865a155774d7df14c3d
Max B <maxbilov@gmail.com>
2020-05-22 18:19:01 +0200

    finished episodeitem styling first version

--- src/components/EpisodeItem.js
+++ src/components/EpisodeItem.js
@@ -1,6 +1,7 @@
 // vim: ft=javascriptreact
 import React from 'react';
 import './EpisodeItem.css';
+import sound from './schumann.mp3';
 
 const EpisodeItem = ({
   data: {
@@ -20,12 +21,15 @@ const EpisodeItem = ({
 
   return (
     <div className="episode-item">
-      <div className="details">
+      <div className="upper">
         <div className="date">{modDate}</div>
         <div className="duration">{duration}</div>
         <div className="title">{title.toUpperCase()}</div>
       </div>
-      <div className="subtitle">{subtitle}</div>
+      <div className="lower">
+        <audio controls src={sound} type="audio/mp3"></audio>
+        <div className="subtitle">{subtitle}</div>
+      </div>
     </div>
   );
 };
